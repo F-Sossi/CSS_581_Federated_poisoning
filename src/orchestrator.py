46585d31de6619a235a012b7b8a978fd13f74d9d
jgnewton <jeremygnewton@gmail.com>
2023-11-20 17:20:05 -0800

    cleanup ATTACK variable

--- src/orchestrator.py
+++ src/orchestrator.py
@@ -6,9 +6,9 @@ import time
 
 print('Running Orchestrator (testJGN)')
 # Parameters
-NUM_TOTAL_CLIENTS = 5
-MAX_MALICIOUS_CLIENTS = 2
-NUM_ROUNDS = 5
+NUM_TOTAL_CLIENTS = 2
+MAX_MALICIOUS_CLIENTS = 1
+NUM_ROUNDS = 1
 RESULTS_DIR = "../experiment_results2"
 
 """
@@ -16,23 +16,23 @@ random_flip
 constant_flip_X # substitute X with the offset (if 0 or 10 labels will be unchanged)
 targeted_TXTY  # substitute X with the label to be changed, Y the label it is changed to
 """
-ATTACK='targeted_T1T2'
+#ATTACK='targeted_T1T2'
 
 
 # Function to start the Flower server
-def start_server(num_rounds, output_file, ATTACK):
-    cmd = ["python", "server.py", "--rounds", str(num_rounds), "--output", output_file, '--attack', ATTACK]
+def start_server(num_rounds, output_file, attack):
+    cmd = ["python", "server.py", "--rounds", str(num_rounds), "--output", output_file, '--attack', attack]
     subprocess.run(cmd)
 
 
 # Function to start a Flower client
-def start_client(is_malicious=False, attack='none',client_id=0,round_number=0):
+def start_client(is_malicious=False, attack='none', client_id=0, round_number=0):
     env = os.environ.copy()
     # print("Starting client. Malicious:", is_malicious)
     env["IS_MALICIOUS"] = "1" if is_malicious else "0"
-    env["ATTACK"]=str(attack)
-    env["CLIENT_ID"]=str(client_id)
-    env["ROUND"]=str(round_number)
+    env["ATTACK"] = str(attack)
+    env["CLIENT_ID"] = str(client_id)
+    env["ROUND"] = str(round_number)
     cmd = ["python", "client.py"]
     subprocess.Popen(cmd, env=env)
     # print("Client finished.")
@@ -40,32 +40,35 @@ def start_client(is_malicious=False, attack='none',client_id=0,round_number=0):
 
 # Main function to orchestrate the server and clients
 def main():
-    ATTACK=sys.argv[1]
-    print(ATTACK)
+    if len(sys.argv) > 1:
+        attack = sys.argv[1]
+    else:
+        attack = 'random_flip'
+    print('orchestrator main, attack type:', attack)
 
     for num_malicious in range(MAX_MALICIOUS_CLIENTS + 1):
     # For testing
     #for num_malicious in range(1,2):
-        print(f"Running experiment with {num_malicious} malicious clients, attack type:", ATTACK)
+        print(f"Running experiment with {num_malicious} malicious clients, attack type:", attack)
 
         # Start the server in a separate thread
-        output_file = os.path.join(RESULTS_DIR, f"{ATTACK}results_{num_malicious}_malicious.json")
-        server_thread = threading.Thread(target=start_server, args=(NUM_ROUNDS, output_file, ATTACK))
+        output_file = os.path.join(RESULTS_DIR, f"{attack}results_{num_malicious}_malicious.json")
+        server_thread = threading.Thread(target=start_server, args=(NUM_ROUNDS, output_file, attack))
         server_thread.start()
         time.sleep(5)  # Wait for the server to initialize
 
         # Start clients
         client_threads = []
         for i in range(NUM_TOTAL_CLIENTS):
-            client_id=i
-            round_number=num_malicious
+            client_id = i
+            round_number = num_malicious
             is_malicious = i < num_malicious
             if not is_malicious:
-                attack_type='none'
+                attack_type = 'none'
             else:
-                attack_type=ATTACK
+                attack_type = attack
             print('creating client', 'malicious:',is_malicious, ', attack_type:', attack_type)
-            client_thread = threading.Thread(target=start_client, args=(is_malicious,ATTACK,client_id,round_number,))
+            client_thread = threading.Thread(target=start_client, args=(is_malicious,attack_type,client_id,round_number,))
             client_threads.append(client_thread)
             client_thread.start()
 
