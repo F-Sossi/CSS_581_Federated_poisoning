d9491b13ac686faca05d85e8a4846dd0eae5a2e5
jgnewton <jeremygnewton@gmail.com>
2023-12-04 11:46:41 -0800

    random experiment

--- src/main.py
+++ src/main.py
@@ -9,10 +9,10 @@ from plotter import run_plotter
 print('Running Orchestrator (testJGN)')
 # Parameters
 #Changing the Params for testing
-NUM_TOTAL_CLIENTS = 3
+NUM_TOTAL_CLIENTS = 6
 MAX_MALICIOUS_CLIENTS = 3
 
-NUM_ROUNDS = 3
+NUM_ROUNDS = 10
 RESULTS_DIR = "../experiment_results"
 
 # An experiment ID
